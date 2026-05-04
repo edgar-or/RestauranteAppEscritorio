8a0ac771d8e34bf55aba0652c4620bbc66a682c7
Emilio <cuentadeusaemilio@gmail.com>
2026-05-04 13:30:35 -0600

    Conexion Emilio

--- src/main/java/dao/conexion/Conexion.java
+++ src/main/java/dao/conexion/Conexion.java
@@ -13,9 +13,15 @@ import java.sql.DriverManager;
  */
 public class Conexion {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/RestauranteAppEscritorio";
+    //Edgar
+   /* private static final String URL = "jdbc:postgresql://localhost:5432/RestauranteAppEscritorio";
     private static final String USER = "postgres";
-    private static final String PASS = "eaadmin76rag19$";
+    private static final String PASS = "eaadmin76rag19$";*/
+       
+    //Emilio
+    private static final String URL = "jdbc:postgresql://localhost:5432/APE";
+    private static final String USER = "postgres";
+    private static final String PASS = "pasen el sepe";
 
     public static Connection getConnection() throws Exception {
         return DriverManager.getConnection(URL, USER, PASS);
