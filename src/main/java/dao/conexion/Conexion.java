e6d8b6c9e74152442158920f8ed5e8b100944fc9
edgar <ayalaalvarezedgarorlando@gmail.com>
2026-05-04 13:27:12 -0600

    correccion de login

--- src/main/java/dao/conexion/Conexion.java
+++ src/main/java/dao/conexion/Conexion.java
@@ -13,7 +13,7 @@ import java.sql.DriverManager;
  */
 public class Conexion {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/RestauranteAppEscritorio";
+    private static final String URL = "jdbc:postgresql://localhost:5432/RESTAURANTEAPPESC";
     private static final String USER = "postgres";
     private static final String PASS = "eaadmin76rag19$";
 
