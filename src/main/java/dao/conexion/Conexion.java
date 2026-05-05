2293194e6490b86fcef458463b6ef8790100cc0f
edgar <ayalaalvarezedgarorlando@gmail.com>
2026-05-04 22:04:09 -0600

    consulta Bar muestra datos en vista

--- src/main/java/dao/conexion/Conexion.java
+++ src/main/java/dao/conexion/Conexion.java
@@ -13,26 +13,25 @@ import java.sql.DriverManager;
  */
 public class Conexion {
     
-     private static final String URL = "jdbc:postgresql://localhost:5432/RestauranteAppEscritorio";
-    private static final String USER = "postgres";
-    private static final String PASS = "Rene";
-
-//    private static final String URL = "jdbc:postgresql://localhost:5432/RestauranteAppEscritorio";
+    
+    //Rene
+//     private static final String URL = "jdbc:postgresql://localhost:5432/RestauranteAppEscritorio";
 //    private static final String USER = "postgres";
-//    private static final String PASS = "eaadmin76rag19$";
+//    private static final String PASS = "Rene";
+
+ 
 
 //IRVIN
 //    private static final String URL = "jdbc:postgresql://localhost:5432/restaurante";
 //    private static final String USER = "postgres";
 //    private static final String PASS = "6074";
 
-//    private static final String URL = "jdbc:postgresql://localhost:5432/RestauranteAppEscritorio";
-//    private static final String USER = "postgres";
-//    private static final String PASS = "eaadmin76rag19$";
 
-    /* private static final String URL = "jdbc:postgresql://localhost:5432/RESTAURANTEAPPESC";
+    private static final String URL = "jdbc:postgresql://localhost:5432/RESTAURANTEAPPESC";
     private static final String USER = "postgres";
-    private static final String PASS = "eaadmin76rag19$";*/
+    private static final String PASS = "eaadmin76rag19$";
+    
+    
     //Emilio
 //    private static final String URL = "jdbc:postgresql://localhost:5432/APE";
 //    private static final String USER = "postgres";
