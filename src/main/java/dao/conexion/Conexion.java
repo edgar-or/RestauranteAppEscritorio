c9b38fcb827bb4d2a40e90dc1aa50152d496ed5c
edgar <ayalaalvarezedgarorlando@gmail.com>
2026-05-22 11:39:17 -0600

    actualizacion de paqueteria de prpyecto

--- src/main/java/dao/conexion/Conexion.java
+++ src/main/java/dao/conexion/Conexion.java
@@ -13,9 +13,9 @@ import java.sql.DriverManager;
  */
 public class Conexion {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/RestauranteAppEscritorio";
-    private static final String USER = "postgres";
-    private static final String PASS = "Rene";
+//    private static final String URL = "jdbc:postgresql://localhost:5432/RestauranteAppEscritorio";
+//    private static final String USER = "postgres";
+//    private static final String PASS = "Rene";
 
 //    private static final String URL = "jdbc:postgresql://localhost:5432/RestauranteAppEscritorio";
 //    private static final String USER = "postgres";
@@ -39,9 +39,9 @@ public class Conexion {
 //    private static final String USER = "postgres";
 //    private static final String PASS = "eaadmin76rag19$";
 
-//    private static final String URL = "jdbc:postgresql://localhost:5432/RESTAURANTEAPPESC";
-//    private static final String USER = "postgres";
-//    private static final String PASS = "eaadmin76rag19$";
+    private static final String URL = "jdbc:postgresql://localhost:5432/RESTAURANTEAPPESC";
+    private static final String USER = "postgres";
+    private static final String PASS = "eaadmin76rag19$";
 
 //    private static final String URL = "jdbc:postgresql://localhost:5432/APE";
 //    private static final String USER = "postgres";
